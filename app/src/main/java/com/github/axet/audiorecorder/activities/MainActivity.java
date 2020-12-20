06cd7fc5c4e33bed73eb1ca1ac6d1c7484913fa9
Lszh0147 <no@email.com>
2020-12-20 22:14:33 +0800

    权限申请

--- app/src/main/java/com/github/axet/audiorecorder/activities/MainActivity.java
+++ app/src/main/java/com/github/axet/audiorecorder/activities/MainActivity.java
@@ -1,11 +1,13 @@
 package com.github.axet.audiorecorder.activities;
 
+import android.Manifest;
 import android.app.KeyguardManager;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Build;
@@ -13,8 +15,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DividerItemDecoration;
@@ -53,6 +58,8 @@ import com.github.axet.audiorecorder.services.RecordingService;
 import org.json.JSONException;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatThemeActivity {
     public final static String TAG = MainActivity.class.getSimpleName();
@@ -67,6 +74,10 @@ public class MainActivity extends AppCompatThemeActivity {
 
     ScreenReceiver receiver;
     EncodingDialog encoding;
+
+    String[] permission = {Manifest.permission.READ_PHONE_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO};
+    List<String> mPermissionList = new ArrayList<>();
 
     public static void startActivity(Context context) {
         context.startActivity(new Intent(context, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
@@ -471,7 +482,53 @@ public class MainActivity extends AppCompatThemeActivity {
         } catch (Exception e) {
             Log.e(TAG, "error", e);
         }
+        checkPermission();
     }
+
+    //授权服务
+    private void checkPermission() {
+        for (int i = 0; i < permission.length; i++) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, permission[i]) != PackageManager.PERMISSION_GRANTED) {
+                mPermissionList.add(permission[i]);
+            }
+        }
+        if (!mPermissionList.isEmpty()) {
+//            Toast.makeText(MainActivity.this, "必须授予所有权限！", Toast.LENGTH_LONG).show();
+            String[] permissions = mPermissionList.toArray(new String[mPermissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+//            ErrorDialog.Error(MainActivity.this, "必须授予所有权限！");
+        } else {
+            try {
+                storage.migrateLocalStorage();
+            } catch (RuntimeException e) {
+                ErrorDialog.Error(MainActivity.this, e);
+            }
+            recordings.load(false, null);
+            checkPending();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            boolean hasAllPermision = true;
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {//用户拒绝授权的权限
+                    //判断是否勾选禁止后不再询问
+                    boolean showRequestPermission = ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, permissions[i]);
+                    if (!showRequestPermission) {
+                        hasAllPermision = false;
+                    }
+                }
+            }
+            if (!hasAllPermision){
+                ErrorDialog.Error(MainActivity.this, "缺少必要权限\n你选择了禁止后不再询问，请主动授予 。");
+//                        Toast.makeText(MainActivity.this, "权限未申请", Toast.LENGTH_LONG).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
 
     void checkPending() {
         if (storage.recordingPending()) {
@@ -627,25 +684,25 @@ public class MainActivity extends AppCompatThemeActivity {
         return -1;
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case RESULT_PERMS:
-                if (Storage.permitted(MainActivity.this, permissions)) {
-                    try {
-                        storage.migrateLocalStorage();
-                    } catch (RuntimeException e) {
-                        ErrorDialog.Error(MainActivity.this, e);
-                    }
-                    recordings.load(false, null);
-                    checkPending();
-                } else {
-                    Toast.makeText(this, R.string.not_permitted, Toast.LENGTH_SHORT).show();
-                }
-                break;
-        }
-    }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        switch (requestCode) {
+//            case RESULT_PERMS:
+//                if (Storage.permitted(MainActivity.this, permissions)) {
+//                    try {
+//                        storage.migrateLocalStorage();
+//                    } catch (RuntimeException e) {
+//                        ErrorDialog.Error(MainActivity.this, e);
+//                    }
+//                    recordings.load(false, null);
+//                    checkPending();
+//                } else {
+//                    Toast.makeText(this, R.string.not_permitted, Toast.LENGTH_SHORT).show();
+//                }
+//                break;
+//        }
+//    }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
